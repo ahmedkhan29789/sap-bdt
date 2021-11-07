34ce80c9eadfb7a1b4ba3f7c864a82bf2e2356f1
Ahmed Ali Khan <ahmed_ali_khan@epam.com>
2021-11-07 18:08:30 +0000

    Added managed router and UI
    Signed-off-by: Ahmed Ali Khan <ahmed_ali_khan@epam.com>

---
+++ app/sap_bdt/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("com.sap.bdt.sapbdt.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
